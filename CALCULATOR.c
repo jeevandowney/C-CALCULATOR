717ec638671dfa9ce50173ead10e1e47bd1210fb
Jeevan <57611618+jeevandowney@users.noreply.github.com>
2020-07-20 10:17:00 +0530

    Add files via upload

---
+++ CALCULATOR.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+int main()
+{
+  /* INITIALIZATION */
+  int num1,num2;
+  float results;
+  char operator;
+
+  /* TO ENTER AND READ THE FIRST NUMBER */
+  printf("Enter the first number " );
+  scanf("%d",&num1);
+
+  /* TO ENTER AND READ THE SECOND NUMBER */
+  printf("Enter the second number " );
+  scanf("%d",&num2);
+
+  /* TO ENTER AND READ THE OPERATOR */
+  printf("Enter the operator (+,-,*,/) : ");
+  scanf(" %c",&operator);
+
+  switch (operator)
+  {
+    /* FOR ADDITION */
+    case '+':
+    results = num1+num2;
+    break;
+
+    /* FOR SUBRACTION */
+    case '-':
+    results = num1-num2;
+    break;
+
+    /* FOR MULTIPLICATION */
+    case'*':
+    results = num1*num2;
+    break;
+
+    /* FOR DIVISION */
+    case '/':
+    results = num1/num2;
+    break;
+
+    /* IF INVALID OPERATOR IS ENTERED */
+    default:
+    printf("INVALID OPERATOR");
+  }
+  /* TO PRINT THE RESULT */
+  printf("%d %c %d = %f",num1,operator,num2,results);
+  return 0;
+}
